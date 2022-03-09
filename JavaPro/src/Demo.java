e911f1a34bec98615964ada1b0e8bc9d27932727
Shetty, Pragati <shetty.pragati@principal.com>
2022-03-09 09:59:36 +0530

    method reference has been added in java project

--- JavaPro/src/Demo.java
+++ JavaPro/src/Demo.java
@@ -1,10 +1,11 @@
-
+import pack1.MyInteface
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		MyInterfcae ref = System.out::println;
+		ref.disp("hello there");
 	}
 
 }
